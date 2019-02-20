0ee1e9f735bd9f5e0e9f18688f9a74093f867c6b
phoebekaranja <phoebemurish@gmail.com>
2019-02-20 23:07:51 +0300

    making  changes

--- app/models.py
+++ app/models.py
@@ -1,6 +1,25 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
-    Cleaning= db.relationship("Cleaning", backref="user", lazy="dynamic")
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+    blogs = db.relationship("Blog", backref="user", lazy="dynamic")
+    comment = db.relationship("Comments", backref="user", lazy ="dynamic")
+
+
+
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -18,22 +37,61 @@ class User(UserMixin,db.Model):
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-class Emplo(db.Model):
-    '''
-    comment class that create instance of comment
-    '''
-    __tablename__ = 'comment'
 
-    #add columns
-    id = db.Column(db. Integer, primary_key=True)
-    comment_name = db.Column(db.String(255))
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+class contact(db.Model):
+    '''
+    defines the table instance of our contact table
+    '''
+    __tablename__= 'contact'
+
+    id = db.Column(db.Integer, primary_key=True)
+    Blog_post = db.Column(db.String)
     date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    category = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    cleaning_id = db.Column(db.Integer, db.ForeignKey("cleaning.id"))
 
-    def save_comment(self):
+    def save_contact(self):
         '''
-        save the comment per blog
+        function to save blog
         '''
         db.session.add(self)
         db.session.commit()
+
+    @classmethod
+    def clear_contacts(cls):
+        '''
+        function that clears all the contacts in the form after submission
+        '''
+        user.all_contacts.clear()
+
+    @classmethod
+    def get_users(cls,id):
+        '''
+        function that gets particular users when requested by date application
+        '''
+        contacts = Contact.query.order_by(Contact.date_posted.desc()).all()
+        return contacts
+
+class worker(db.Model):
+    '''
+    worker class that create instance of worker
+    '''
+    __tablename__ = 'worker'
+
+    #add columns
+    id = db.Column(db. Integer, primary_key=True)
+    worker_name = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    worker_id = db.Column(db.Integer, db.ForeignKey("worker.id"))

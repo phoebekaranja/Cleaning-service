f25f4e2835b87f25bc2758ee53a668f034eb48b9
phoebekaranja <phoebemurish@gmail.com>
2019-02-20 10:43:25 +0300

    initial commits

---
+++ app/main/views.py
@@ -0,0 +1,6 @@
+@main.route('/')
+def index():
+    '''
+    view root page function that returns the index page
+    '''
+    title = 'Home - Welcome to The Best Blog Site Worldwide You Think of It We help share It.'

6e8c64b1952056f043fd0d8cca778915bb5be4b4
phoebekaranja <phoebemurish@gmail.com>
2019-02-20 10:54:13 +0300

    added an virtualtag and made some initials changes to views.py

--- app/main/views.py
+++ app/main/views.py
@@ -3,4 +3,5 @@ def index():
     '''
     view root page function that returns the index page
     '''
-    title = 'Home - Welcome to The Best Blog Site Worldwide You Think of It We help share It.'
+    title = 'Home - Welcome to The Best cleaning Site Worldwide You Think of It We help share It.'
+    return render_template('index.html',title = title)
